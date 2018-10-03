5a35fa948f46b533a1597a4e9ac581230fe4808d
unknown <powoznia@CE24244.corp.capgemini.com>
2018-10-03 12:09:52 +0200

    first commit

---
+++ src/main/java/jstk/jstk_p/App.java
@@ -0,0 +1,13 @@
+package jstk.jstk_p;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
